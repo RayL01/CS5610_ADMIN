f387d6be01de56d7b26fd23440eacfec7ff4491e
Ray Liu <wooshadyyy@gmail.com>
2024-03-13 22:26:33 -0400

    page title updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Poppins } from "next/font/google";
 import { Providers } from "@/redux/provider";
 
 export const metadata: Metadata = {
-  title: "Vakker Dental",
+  title: "Mern Shop",
 };
 
 const poppins = Poppins({
